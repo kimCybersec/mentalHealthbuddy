327377ab012ad654c014392eb92108827b6833a7
Rai Kim <kim@host.client.com>
2025-06-08 19:15:32 +0300

    working backend

--- api/services/firestore.py
+++ api/services/firestore.py
@@ -1,13 +1,17 @@
 import os
 import json
+import base64
 from firebase_admin import credentials, firestore, initialize_app
 
 cred_data = os.environ.get("GOOGLE_CREDENTIALS")
 
 if cred_data:
-    cred_json = json.loads(cred_data)
+    # 🔥 Decode from base64 to JSON string
+    decoded = base64.b64decode(cred_data).decode("utf-8")
+    cred_json = json.loads(decoded)
     cred = credentials.Certificate(cred_json)
 else:
+    # Optional fallback for local testing
     cred = credentials.Certificate("mensmentalhealth.json")
 
 initialize_app(cred)
