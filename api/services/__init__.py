a4ad39abd9b742690b812b59ac9a5f45051df51f
Rai Kim <kim@host.client.com>
2025-06-08 14:51:17 +0300

    no backend

--- api/services/__init__.py
+++ api/services/__init__.py
@@ -1,8 +1,7 @@
 from services.openaiHelper import generate_response
-from utils.safetyChecker import SafetyChecker, checkSafety
+from utils.safetyChecker import is_safe
 
 __all__ = [
     "generate_response",
-    "SafetyChecker",
-    "checkSafety"
+    "is_safe"
 ]
