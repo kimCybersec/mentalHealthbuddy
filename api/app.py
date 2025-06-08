5d3da4348310e01c7e295368491994104885cce0
Rai Kim <kim@host.client.com>
2025-06-08 14:28:00 +0300

    no backend

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask 
-from api.routes.chat import chat_bp 
+from routes.chat import chat_bp 
 from flask_cors import CORS
 
 app = Flask(__name__) 
--- api/services/__init__.py
+++ api/services/__init__.py
@@ -1,5 +1,5 @@
 from services.openaiHelper import OpenAIClient
-from backend.api.utils.safetyChecker import SafetyChecker, checkSafety
+from utils.safetyChecker import SafetyChecker, checkSafety
 
 __all__ = [
     "OpenAIClient",
