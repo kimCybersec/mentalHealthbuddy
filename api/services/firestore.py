8bed682ed208cea8b0dce3b3c3eaca925d0feda3
Rai Kim <kim@host.client.com>
2025-06-08 16:20:03 +0300

    no backend

--- api/services/firestore.py
+++ api/services/firestore.py
@@ -1,18 +1,27 @@
-import firebase_admin 
-from firebase_admin import credentials, firestore 
-import os 
-import base64
+import os
+import json
+from firebase_admin import credentials, firestore, initialize_app
 
-if not firebase_admin._apps: 
-    cred_data = base64.b64decode(os.getenv("GOOGLE_CREDENTIALS")) 
-    cred = credentials.Certificate.from_service_account_info(eval(cred_data)) 
-    firebase_admin.initialize_app(cred)
+cred_data = os.environ.get("GOOGLE_CREDENTIALS")
+
+if cred_data:
+    cred_json = json.loads(cred_data)
+    cred = credentials.Certificate(cred_json)
+else:
+    cred = credentials.Certificate("mensmentalhealth.json")
+
+initialize_app(cred)
 
 db = firestore.client()
 
-def save_chat(session_id, user_message, bot_response): 
-    db.collection("chats").add({ "session_id": session_id, "user": user_message, "bot": bot_response, "timestamp": firestore.SERVER_TIMESTAMP })
+def save_chat(session_id, user_message, bot_response):
+    db.collection("chats").add({
+        "session_id": session_id,
+        "user": user_message,
+        "bot": bot_response,
+        "timestamp": firestore.SERVER_TIMESTAMP
+    })
 
-def get_chat_history(session_id): 
-    docs = db.collection("chats").where("session_id", "==", session_id).order_by("timestamp").stream() 
+def get_chat_history(session_id):
+    docs = db.collection("chats").where("session_id", "==", session_id).order_by("timestamp").stream()
     return [{"user": doc.to_dict()['user'], "bot": doc.to_dict()['bot']} for doc in docs]
