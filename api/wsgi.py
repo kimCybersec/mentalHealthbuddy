e2026ef23f874b1c809fc082683b12e72c5fedc2
kimCybersec <kimutairainner@gmail.com>
2025-06-09 15:41:10 +0300

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,9 +1,9 @@
-from .app import app 
+from api.app import app 
 from flask import send_from_directory
 
 @app.route('/')
 def serveIndex():
-    return send_from_directory("frontend/templates", "index.html")
+    return send_from_directory("frontend/public", "index.html")
 
 @app.route('/<path:path>')
 def serveStatic(path):
