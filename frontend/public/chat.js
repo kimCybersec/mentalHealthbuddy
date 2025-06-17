2a073649b150847a0d231976f6586c7b3f516ba5
Rai Kim <kim@host.client.com>
2025-06-18 00:21:22 +0300

    .

--- frontend/public/chat.js
+++ frontend/public/chat.js
@@ -17,7 +17,7 @@ function addMessage(role, content) {
   msgWrapper.className = `message-wrapper ${role}`;
 
   const avatar = document.createElement("img");
-  avatar.src = role === "user" ? "frontend/img/user.png" : "/static/img/bot.png";
+  avatar.src = role === "user" ? "/frontend/img/user.png" : "/static/img/bot.png";
   avatar.alt = role;
   avatar.className = "avatar";
 
